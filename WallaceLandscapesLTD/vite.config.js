f316708868cb4cf82be90005415c6440d621d8e2
gabe-woolaver <woolaverg@gmail.com>
2025-10-02 13:50:36 -0300

    Add files via upload

---
+++ WallaceLandscapesLTD/vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  // IMPORTANT: must match your GitHub repo name exactly
+  base: '/WallaceLandscapesLtd/',
+});
